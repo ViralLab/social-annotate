eccc47d713ea6bdcf27b1596fba6c778f4a47279
AliNajafi1998 <najafi1998ali@gmail.com>
2026-04-18 20:57:44 +0300

    flask server updated

--- flask-collect/main.py
+++ flask-collect/main.py
@@ -4,7 +4,7 @@ import json
 from flask import Flask, Response
 from flask import request, jsonify
 
-from flask.ext.cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/response": {"origins": "*"}})
@@ -13,11 +13,11 @@ app.config["DEBUG"] = True
 
 class DataStorage(object):
 
-    def __init__(self, filename='reponses.jsons'):
+    def __init__(self, filename='responses.jsonl'):
         self.filename = filename
 
     def appendResponse(self, response):
-        with open(self.filename, 'a') as fl:
+        with open(self.filename, 'a', encoding='utf-8') as fl:
             fl.write('{}\n'.format(json.dumps(response)))
 
 
@@ -26,12 +26,16 @@ DS = DataStorage()
 @app.route('/response', methods=['POST'])
 @cross_origin(origin='*',headers=['access-control-allow-origin','Content-Type'])
 def collect_response():
-    resp = json.loads(request.data)
-    print('[New data] => {}'.format(json.dumps(resp)))
-    DS.appendResponse(resp)
-    return Response(status=200)
+    resp = request.get_json(force=True, silent=True)
+    if resp is None and request.data:
+        resp = json.loads(request.data.decode('utf-8'))
+    
+    if resp:
+        print('[New data] => {}'.format(json.dumps(resp)))
+        DS.appendResponse(resp)
+        return Response(status=200)
+    return Response(status=400)
 
 
 if __name__ == '__main__':
-    
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5050)
